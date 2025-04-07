7017f9e85efa08f8a066d48fd249eecd74a13ce5
wintrover <wintrover@gmail.com>
2025-04-07 21:05:58 +0900

    Docker에서 네이티브 Python 배포로 변경

--- build.sh
+++ build.sh
@@ -1,21 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # 오류 발생 시 스크립트 종료
 set -o errexit
 
-# logs 디렉토리 생성
-mkdir -p logs
-mkdir -p static
-mkdir -p frontend
-mkdir -p staticfiles
+echo "빌드 스크립트 시작..."
 
-# 필요한 패키지 설치
+# 파이썬 버전 확인
+python --version
+
+# 종속성 설치
+echo "종속성 설치 중..."
 pip install -r requirements.txt
-pip install 'whitenoise[brotli]' waitress django-extensions werkzeug pyOpenSSL
 
 # 정적 파일 수집
-python manage.py collectstatic --noinput
+echo "정적 파일 수집 중..."
+python manage.py collectstatic --no-input
 
-# 데이터베이스 마이그레이션
-python manage.py migrate --noinput
+# 필요한 디렉토리 생성
+echo "디렉토리 생성 중..."
+mkdir -p logs static_dev frontend static_prod
 
 echo "빌드 완료!" 
