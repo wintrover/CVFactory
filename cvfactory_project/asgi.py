e2c69853c38dbc612acb4fa834b16821847ee609
wintrover <wintrover@gmail.com>
2025-05-09 20:04:40 +0900

    Force DJANGO_SETTINGS_MODULE to 'cvfactory_project.settings'

--- cvfactory_project/asgi.py
+++ cvfactory_project/asgi.py
@@ -11,6 +11,5 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cvfactory_project.settings")
-
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cvfactory_project.settings'
 application = get_asgi_application()
