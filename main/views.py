4bf1ad73188d3a167f3d5506c53d16af8f432e5e
wintrover <wintrover@gmail.com>
2025-05-12 20:11:32 +0900

    Add health check endpoint

--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def health_check(request):
+    return HttpResponse("OK")
