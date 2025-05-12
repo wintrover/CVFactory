860871e5d09a5a9de1592fcb6c6cb66b71c605f9
wintrover <wintrover@gmail.com>
2025-05-12 21:42:40 +0900

    chore: Comment out STATICFILES_STORAGE for diagnosis

--- main/settings.py
+++ main/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = '/app/staticfiles/'
