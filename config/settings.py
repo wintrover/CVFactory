95e05f8c01a59051a074ba6c296bca8a9784a8c3
wintrover <wintrover@gmail.com>
2025-05-24 17:59:52 +0900

    \fix: Add csp to INSTALLED_APPS"

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',  # Add corsheaders
+    'csp',  # django-csp 앱 추가
     # Your apps here
     'core',
     'compressor', # django-compressor 추가
